1d1e64861f0939c3529a757dca96ec39a4597966
Simen A. W. Olsen <cobraz@cobraz.no>
2021-04-11 00:51:47 +0200

    feat: Add Billing System definitions (#5)
    Co-authored-by: cobraz <cobraz@users.noreply.github.com>

---
+++ src/generated/btools/account/v1/account.ts
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import { Address } from '../../../btools/type/address';
+import { Timestamp } from '../../../google/protobuf/timestamp';
+
+export interface Account {
+  /**
+   *  Resource name, for example, "accounts/bjerkio"
+   */
+  name: string;
+  /**
+   *  Name of customer
+   */
+  title: string;
+  addresses: Address[];
+  /**
+   *  Vat ID, organization number (Company Identification Number)
+   */
+  vatIdentification: string;
+  createTime: Timestamp | undefined;
+  updateTime: Timestamp | undefined;
+}
